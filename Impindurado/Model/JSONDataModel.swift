bad9a7dbc259dcbbe6bd974167900d5af0163892
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2021-08-11 20:42:30 -0300

    Add JSONData model and word structure

---
+++ Impindurado/Model/JSONDataModel.swift
@@ -0,0 +1,35 @@
+//
+//  JSONDataModel.swift
+//  Impindurado
+//
+//  Created by Ronaldo Gomes on 11/08/21.
+//
+
+import Foundation
+
+class JSONDataModel {
+    private lazy var words: [Word] = readAllWords()
+    
+    internal func readAllWords(for jsonFile: String = "jsonFile") -> [Word] {
+        let pathUrl = Bundle.main.url(forResource: jsonFile, withExtension: "json")!
+        let jsonData = try? Data(contentsOf: pathUrl)
+        guard let data = jsonData else {
+            fatalError("Json could not be read")
+        }
+        
+        let words: [Word]
+        do {
+            words = try JSONDecoder().decode([Word].self, from: data)
+        } catch {
+            fatalError("Json could not parsed")
+        }
+        return words
+    }
+    
+    func getRandomWord() -> Word {
+        guard let randomWord = words.randomElement() else {
+            fatalError("Words is empty")
+        }
+        return randomWord
+    }
+}
