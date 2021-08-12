08641ad09ea7b7b06e31e8748fdb89552196a199
Vinicius Mesquita <viniciuscoelho.ti@gmail.com>
2021-08-12 11:59:26 -0300

    Add game scene and keyboard view

---
+++ Impindurado/Scenes/Game/GameViewController.swift
@@ -0,0 +1,40 @@
+//
+//  
+//  GameViewController.swift
+//  Impindurado
+//
+//  Created by Vinicius Mesquita on 10/08/21.
+//
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    let mainView = GameView()
+    var viewModel: GameViewModel?
+
+    init(viewModel: GameViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    override func loadView() {
+        self.view = mainView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mainView.keyboardView.delegate = self
+    }
+}
+
+extension GameViewController: KeyboardDelegate {
+    func didSelectKey(key: String) {
+        print(key)
+    }
+}
