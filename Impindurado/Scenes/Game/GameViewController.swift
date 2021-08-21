433bef9b5cc71e49b59537ee13401273b3208d1b
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2021-08-21 15:31:18 -0300

    Add json file

--- Impindurado/Scenes/Game/GameViewController.swift
+++ Impindurado/Scenes/Game/GameViewController.swift
@@ -12,7 +12,7 @@ import UIKit
 class GameViewController: UIViewController {
 
     let mainView = GameView()
-    var viewModel: GameViewModel?
+    var viewModel: GameViewModel
     var coordinator: GameCoordinator?
     let model = WordsModel()
 
